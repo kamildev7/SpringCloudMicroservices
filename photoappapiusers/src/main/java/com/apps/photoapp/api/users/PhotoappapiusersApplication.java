de4145360fc76c3829b849ee32ad581a4f2bc320
Kamil Komenda <k.93kamil@gmail.com>
2019-07-09 00:05:05 +0200

    Added Photo Api Users microservice

---
+++ photoappapiusers/src/main/java/com/apps/photoapp/api/users/PhotoappapiusersApplication.java
@@ -0,0 +1,13 @@
+package com.apps.photoapp.api.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoappapiusersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoappapiusersApplication.class, args);
+    }
+
+}
---
+++ photoappapiusers/src/test/java/com/apps/photoapp/api/users/PhotoappapiusersApplicationTests.java
@@ -0,0 +1,16 @@
+package com.apps.photoapp.api.users;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PhotoappapiusersApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
