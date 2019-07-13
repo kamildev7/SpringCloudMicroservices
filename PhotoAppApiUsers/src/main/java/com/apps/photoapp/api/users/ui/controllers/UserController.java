848ad4659a0e99d0bb108063822fd1ff59583833
Kamil Komenda <k.93kamil@gmail.com>
2019-07-13 19:13:23 +0200

    Added gateway ip in properties, specified produce/consume request method

--- PhotoAppApiUsers/src/main/java/com/apps/photoapp/api/users/ui/controllers/UserController.java
+++ PhotoAppApiUsers/src/main/java/com/apps/photoapp/api/users/ui/controllers/UserController.java
@@ -9,6 +9,7 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,7 +37,8 @@ public class UserController {
         return "Working on port " + env.getProperty("local.server.port");
     }
 
-    @PostMapping
+    @PostMapping(consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE},
+                produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<CreateUserResponse> createUser(@Valid @RequestBody final CreateUserRequest userDetails) {
 
         ModelMapper modelMapper = new ModelMapper();
